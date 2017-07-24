131ed552be781593477ea3b110cce23a00f2401d
Bianca CSSI <bianca.lara2017@gmail.com>
2017-07-24 16:04:34 -0700

    Yes

--- resources/game.js
+++ resources/game.js
@@ -2,3 +2,14 @@ var canvas = document.getElementById("room");
 var ctx = canvas.getContext("2d");
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(0, 0, 150, 75);
+
+function setup(){
+  $('body').keydown(whileMoving);
+  $('body').keyup(whileMoving);
+}
+
+function whileMoving(e){
+  console.log(e.currentTarget)
+}
+
+$(document).ready(setup);
