89792781401aced45fc803a84640cdbe4fe81c4c
Bianca CSSI <bianca.lara2017@gmail.com>
2017-07-24 09:55:46 -0700

    Bianca's Changes

--- main.py
+++ main.py
@@ -16,16 +16,32 @@ from google.appengine.ext import ndb
 env = jinja2.Environment(loader = jinja2.FileSystemLoader(os.path.dirname(__file__)
 + '/templates'))
 
+class User(ndb.Model):
+    username = ndb.StringProperty()
+    password = ndb.StringProperty()
 class HomePage(webapp2.RequestHandler):
     def get(self):
+        invalid = False
+        user_key = ndb.Key('User', self.request.get('username'), 'User', self.request.get('password'))
+        user = user_key.get()
+        if not user:
+            invalid = True
+            self.redirect('/')
+        else:
+            self.redirect('/game')
 
+        template = env.get_template('main.html')
+        var = {'user' : user, 'invalid': invalid}
+        self.response.out.write(template.render(var))
 class NewUserPage(webapp2.RequestHandler):
     def get(self):
     def post(self):
 
+
 class GamePage(webapp2.RequestHandler):
     def get(self):
     def post(self):
+
 app=webapp2.WSGIApplication([
 ('/', HomePage),
 ('/new_user', NewUserPage),
