952b3b1691f01d28280b321aeb446936c11f1df1
Bianca CSSI <bianca.lara2017@gmail.com>
2017-07-27 13:38:13 -0700

    Help

--- resources/game.js
+++ resources/game.js
@@ -77,6 +77,7 @@ function createEnemy(){
   var speed = Math.round(Math.random() * 5 + 8);
   Enemy = [];
   Enemy.push("Bones", 20, attack, speed);
+  $('#enemyH').text(Enemy[1]);
 }
 
 //states will be used to stop funtion whileMoving to work
@@ -240,7 +241,7 @@ function meMagicGif(){
 
 }
 
-function meFastkGif(){
+function meFastGif(){
   $("#character").attr('src', "../resources/swordattack.gif");
 
     setTimeout(function(){
